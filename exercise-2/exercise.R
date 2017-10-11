62f03fb45e778bcd1708fb90b68b0dc19612e169
bch0ng <bchong@uw.edu>
2017-10-11 16:54:54 -0700

    Finished Lab Exercise

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,7 +3,7 @@
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
 CompareLength <- function(a, b) {
-  return (paste("The difference in lengths is", abs(length(a) - length(b))))
+  return(paste("The difference in lengths is", abs(length(a) - length(b))))
 }
 
 # Pass two vectors of different length to your `CompareLength` function
@@ -13,15 +13,15 @@ CompareLength(c(1,2,3), c(1,2,3,4))
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
 DescribeDifference <- function(a, b) {
-  a <- length(a)
-  b <- length(b)
-  if (a > b) {
-    return (paste("Your first vector is longer by", a - b, "elements"))
-  } else if (a < b) {
-    return (paste("Your second vector is longer by", b - a, "elements"))
+  diff <- length(a) - length(b)
+  if (diff > 0) {
+    sentence <- (paste("Your first vector is longer by", abs(diff), "elements"))
+  } else if (diff < 0) {
+    sentence <-  (paste("Your second vector is longer by", abs(diff), "elements"))
   } else {
-    return ("Both vectors have an equal number of elements")
+    sentence <- ("Both vectors have an equal number of elements")
   }
+  return(sentence)
 }
 
 # Pass two vectors to your `DescribeDifference` function
